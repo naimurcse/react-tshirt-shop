0a7d012c818532ed5dff076b773345756a9c2313
naimurcse <2015naim@gmail.com>
2023-12-04 11:42:06 +0600

    received context and displayed according to condition

--- src/component/Nephew/Nephew.jsx
+++ src/component/Nephew/Nephew.jsx
@@ -1,8 +1,17 @@
+import { useContext } from "react";
+import { RingContext } from "../Grandpa/Grandpa";
+
 const Nephew = ({ children }) => {
+   const newRing = useContext(RingContext);
    return (
       <div className="area">
          <h3>Nephew</h3>
          <p>{children}</p>
+         {children === "New Baby" && (
+            <p className="text-red">
+               <strong>{newRing}</strong>
+            </p>
+         )}
       </div>
    );
 };
--- src/component/Niece/Niece.jsx
+++ src/component/Niece/Niece.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import { RingContext } from "../Grandpa/Grandpa";
 
 const Niece = ({ children }) => {
+   const angtiKoti = useContext(RingContext);
    return (
       <div className="area">
          <h3>Niece</h3>
          <p>{children}</p>
+         {children === "Pak Pak" && (
+            <p className="text-red">
+               <strong>{angtiKoti}</strong>
+            </p>
+         )}
       </div>
    );
 };
